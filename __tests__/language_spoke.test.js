b3803678b142bf9167c83c6beee434a65d0a3dd2
David Stinnett <davidstinnett@icloud.com>
2023-06-06 22:14:08 -0400

    added first test

--- __tests__/language_spoke.test.js
+++ __tests__/language_spoke.test.js
@@ -5,3 +5,18 @@ import {
   countryListLookup,
   getResponse,
 } from "../language_spoken.js";
+
+test("convert array of country data objects to array of countries", () => {
+  //arrange
+  const inputObject = [
+    { name: "Argentina", capital: "Buenos Aires" },
+    { name: "Belize", capital: "Belmopan" },
+    { name: "Bolivia", capital: "Sucre" },
+  ];
+  const expectedValue = ["Argentina", "Belize", "Bolivia"];
+
+  //act
+  const actualValue = countryExtractor(inputObject);
+
+  //assertions
+});
